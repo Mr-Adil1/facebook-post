882e6ec8d50040ee15d505a2448354996a521de0
Muhammad Adil <mr.web.developer3@gmsil.com>
2022-04-18 04:26:07 -0700

    changing name and images with loop

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const row1 = document.querySelector('.row-1');
 const inputs = document.getElementById('inputs');
 const userimg = row1.querySelector('img');
-const myimg = document.getElementById('userimg');
+const myimg = document.querySelectorAll('input[type="image"]');
 const clos = row1.querySelector('.clos');
 const file = document.getElementById('file-input');
 const nameinput = document.getElementById('name');
-var myname = document.getElementById('myname');
-console.log(myname);
+var myname = document.getElementsByClassName('myname');
 
 // window.addEventListener('load', () => {
 //     setTimeout(() => {
@@ -23,12 +22,16 @@ userimg.onclick = () => {
 
 
 function username() {
-    myname.innerHTML = nameinput.value;
+    for (var i = 0; i < myname.length; i++) {
+        myname[i].innerHTML = nameinput.value;
+    }
 }
 
 file.addEventListener('change', (event) => {
     var newimg = URL.createObjectURL(event.target.files[0])
     userimg.src = newimg;
-    myimg.src = userimg.src;
+    for (var i = 0; i < myimg.length; i++) {
+        myimg[i].src = newimg;
+    }
     inputs.style.top = '-200px';
-})
+});
